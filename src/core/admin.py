004371b9f59f32b2ac9d564495364cf035aabc8d
AlineDominique <alinedomi.silva@gmail.com>
2020-05-04 18:52:05 -0300

    Add files via upload

---
+++ src/core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Autor,Categorias, Jornal
+
+admin.site.register(Autor)
+admin.site.register(Categorias)
+admin.site.register(Jornal)
---
+++ src/core/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def BootstrapFilterView(request):
+    return render(request, "bootstrap_form.html")
